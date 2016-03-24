81bfa0bbd8bf352d9b65f05183cd4affa9581a1f
Andrew Ayer <agwa@andrewayer.name>
2016-03-23 20:19:39 -0700

    Add ctparsewatch
    It watches for certificates which we can't fully parse

---
+++ cmd/ctparsewatch/main.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"flag"
+	"os"
+
+	"src.agwa.name/ctwatch"
+	"src.agwa.name/ctwatch/ct"
+	"src.agwa.name/ctwatch/cmd"
+)
+
+func DefaultStateDir () string {
+	if envVar := os.Getenv("CTPARSEWATCH_STATE_DIR"); envVar != "" {
+		return envVar
+	} else {
+		return cmd.DefaultStateDir("ctparsewatch")
+	}
+}
+
+var stateDir = flag.String("state_dir", DefaultStateDir(), "Directory for storing state")
+
+func processEntry (scanner *ctwatch.Scanner, entry *ct.LogEntry) {
+	info := ctwatch.EntryInfo{
+		LogUri:		scanner.LogUri,
+		Entry:		entry,
+		IsPrecert:	ctwatch.IsPrecert(entry),
+		FullChain:	ctwatch.GetFullChain(entry),
+	}
+
+	info.CertInfo, info.ParseError = ctwatch.MakeCertInfoFromLogEntry(entry)
+
+	if info.HasParseErrors() {
+		cmd.LogEntry(&info)
+	}
+}
+
+func main() {
+	flag.Parse()
+	cmd.Main(*stateDir, processEntry)
+}
